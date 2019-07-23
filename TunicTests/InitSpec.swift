7f14dc393a5762a43a283f025ba04edcea0a948d
early evening <eve@haut-kultur.org>
2019-07-23 15:32:10 -0700

    improve project structure

---
+++ TunicTests/InitSpec.swift
@@ -0,0 +1,17 @@
+//
+//  InitSpec.swift
+//  TunicTests
+//
+//  Created by Early Evening on 7/23/19.
+//  Copyright © 2019 The Better Fish Orchestra. All rights reserved.
+//
+
+import Foundation
+//import Quick
+//import Nimble
+//
+//class InitSpec: QuickSpec {
+//    override func spec() {
+//
+//    }
+//}
